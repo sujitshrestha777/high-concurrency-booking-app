f0c8db36aa5feca8d73634c1241420ecd86e2e5f
sujit shrestha <sujitshrestha2060@gmail.com>
2026-01-09 20:13:40 +0545

    feat(Navbar): enhance login button with loading state and update Google sign-in prompt

--- apps/web/lib/auth/handleGoogleSignIn.ts
+++ apps/web/lib/auth/handleGoogleSignIn.ts
@@ -2,7 +2,7 @@ import { signIn } from "next-auth/react";
 
 export const handleGoogleSignIn = async () => {
   try {
-    await signIn("google", { callbackUrl: "/" });
+    await signIn("google", { callbackUrl: "/"},{prompt: "select_account" });
   } catch (error) {
     console.error("Error:", error);
     throw error;
